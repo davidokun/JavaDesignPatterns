29daa0e92834e1680ecc099ccd5b051fa2037f4a
davidokun <davidokun99@gmail.com>
2017-01-10 21:29:57 -0500

    Prototype - Adding Client class to test pattern.

---
+++ src/main/java/creational/prototype/GraphicTool.java
@@ -0,0 +1,18 @@
+package creational.prototype;
+
+public class GraphicTool {
+
+    private Graphic prototype;
+
+    public GraphicTool(Graphic prototype) {
+        this.prototype = prototype;
+    }
+
+    protected Graphic createGraphic() {
+        return prototype.clone();
+    }
+
+    public void setPrototype(Graphic prototype) {
+        this.prototype = prototype;
+    }
+}
