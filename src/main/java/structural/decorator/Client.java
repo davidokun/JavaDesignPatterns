946b10738f30ce94527230b94eba1790596ca340
davidokun <davidokun99@gmail.com>
2017-02-01 15:30:44 -0500

    decorator - Adding Client to test pattern.

---
+++ src/main/java/structural/decorator/Client.java
@@ -0,0 +1,13 @@
+package structural.decorator;
+
+public class Client {
+
+    public static void main(String[] args) {
+
+        Window window = new Window();
+        IconWindowDecorator iconWindowDecorator = new IconWindowDecorator(window);
+        ScrollBarWindowDecorator scrollBarWindowDecorator = new ScrollBarWindowDecorator(iconWindowDecorator);
+
+        scrollBarWindowDecorator.draw();
+    }
+}
