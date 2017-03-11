e7d7d56853ac87977bf6ef288da9e272200fdf5e
davidokun <davidokun99@gmail.com>
2017-03-10 19:31:01 -0500

    strategy - Easy ChessAlgorithm implementation.

---
+++ src/main/java/behavioral/strategy/EasyChessAlgorithm.java
@@ -0,0 +1,9 @@
+package behavioral.strategy;
+
+public class EasyChessAlgorithm implements ChessAlgorithm {
+
+    @Override
+    public String calculateNextStep() {
+        return "Easy step...";
+    }
+}
