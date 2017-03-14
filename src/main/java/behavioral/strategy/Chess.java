ebaa32d68f46f11b8b403e9cce36e436146f2fa7
davidokun <davidokun99@gmail.com>
2017-03-13 19:55:01 -0500

    strategy - Main Chess class to use multiple algorithm implementation.

---
+++ src/main/java/behavioral/strategy/Chess.java
@@ -0,0 +1,14 @@
+package behavioral.strategy;
+
+public class Chess {
+
+    private ChessAlgorithm algorithm = new EasyChessAlgorithm();
+
+    public String calculateNextStep() {
+        return algorithm.calculateNextStep();
+    }
+
+    public void setAlgorithm(ChessAlgorithm algorithm) {
+        this.algorithm = algorithm;
+    }
+}
