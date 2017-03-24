f8ac682bf9e445ec1ca7490d5ef1905d8c18d46a
davidokun <davidokun99@gmail.com>
2017-03-23 20:24:06 -0500

    visitor - RenderCartVisitor to only render car part.

---
+++ src/main/java/behavioral/visitor/RenderCarPartVisitor.java
@@ -0,0 +1,9 @@
+package behavioral.visitor;
+
+public class RenderCarPartVisitor implements CarPartVisitor {
+
+    @Override
+    public void visit(CarPart carPart) {
+        System.out.println("Render: " + carPart);
+    }
+}
