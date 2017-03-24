c11c5ffc98faf01d7112c58792482b352113ec64
davidokun <davidokun99@gmail.com>
2017-03-23 20:15:06 -0500

    visitor - PrintCartVisitor to only print car part.

---
+++ src/main/java/behavioral/visitor/PrintCarPartVisitor.java
@@ -0,0 +1,9 @@
+package behavioral.visitor;
+
+public class PrintCarPartVisitor implements CarPartVisitor {
+
+    @Override
+    public void visit(CarPart carPart) {
+        System.out.println("Print: " + carPart);
+    }
+}
