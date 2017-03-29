acb66c3aebdb6e1ce518ec238686caef107ab389
davidokun <davidokun99@gmail.com>
2017-03-28 20:24:06 -0500

    visitor - UpgradeCartVisitor to only upgrade car part.

---
+++ src/main/java/behavioral/visitor/UpgradeCarPartVisitor.java
@@ -0,0 +1,9 @@
+package behavioral.visitor;
+
+public class UpgradeCarPartVisitor implements CarPartVisitor {
+
+    @Override
+    public void visit(CarPart carPart) {
+        System.out.println("Upgrade: " + carPart);
+    }
+}
