f5b5f37bc443aa81cf030b8fc0499ea7025ae90a
davidokun <davidokun99@gmail.com>
2017-04-06 20:55:06 -0500

    observer - Client class to test Observer pattern.

---
+++ src/main/java/behavioral/observer/Client.java
@@ -0,0 +1,16 @@
+package behavioral.observer;
+
+public class Client {
+
+    public static void main(String[] args) {
+
+        Button button = new Button();
+        InputText inputText = new InputText();
+        List list = new List();
+
+        button.attachObserver(inputText);
+        button.attachObserver(list);
+
+        button.clicked();
+    }
+}
