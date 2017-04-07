b0d40ef158b7b0287d483eb9a68d163f0f648cbc
davidokun <davidokun99@gmail.com>
2017-04-06 20:42:06 -0500

    observer - Button class to have observers and notify them a change.

---
+++ src/main/java/behavioral/observer/Button.java
@@ -0,0 +1,28 @@
+package behavioral.observer;
+
+import java.util.ArrayList;
+
+public class Button implements Subject {
+
+    java.util.List<Observer> observers = new ArrayList<>();
+
+    public void clicked() {
+        change();
+    }
+
+
+    @Override
+    public void attachObserver(Observer observer) {
+        this.observers.add(observer);
+    }
+
+    @Override
+    public void detachObserver(Observer observer) {
+        this.observers.remove(observer);
+    }
+
+    @Override
+    public void change() {
+        observers.forEach(Observer::update);
+    }
+}
