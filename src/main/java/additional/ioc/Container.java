dde81282b1b0f93a37177eabe0df26a33c1df277
davidokun <davidokun99@gmail.com>
2017-06-02 18:49:06 -0500

    IoC - Container class to manage dependencies.

---
+++ src/main/java/additional/ioc/Container.java
@@ -0,0 +1,22 @@
+package additional.ioc;
+
+public class Container {
+
+    private ImageService imageService;
+
+    public void init() {
+        imageService = new ImageService();
+        imageService.setFileSystem(new WindowsFileSystem());
+    }
+
+    public ImageService getImageService() {
+        return imageService;
+    }
+
+    public static void main(String[] args) {
+        Container container = new Container();
+        container.init();
+        ImageService imageService = container.getImageService();
+        System.out.println(imageService.readImage());
+    }
+}
